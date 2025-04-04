077937a2f5e4e0f44936fff7766c16365f018251
Sosiggg <salas.ivisusej@gmail.com>
2025-04-04 13:34:21 +0800

    Added path

--- todoproject/urls.py
+++ todoproject/urls.py
@@ -16,8 +16,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.shortcuts import redirect
+
+def redirect_to_api(request):
+    return redirect('/api/')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('todoapp.urls')),
+    path('', redirect_to_api),
 ]
